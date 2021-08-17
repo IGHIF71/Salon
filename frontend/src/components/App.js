dff8fbb8aaf7ad6890c6efcdf4e44264e9c5e8f2
Khalil Robinson <khalil.robinson@autodesk.om>
2021-08-17 17:55:28 -0400

    finish setting up

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import {render} from 'react-dom'
 
 export default function App(){
 
-    return(
-        <h1>Testing React Code</h1>
-    )
+    
+    return(<h1>Fuck react</h1>);
+    
 }
 
 const appDiv = document.getElementById("main");
-render (<App />, appDiv);
+render(<App />, appDiv);
