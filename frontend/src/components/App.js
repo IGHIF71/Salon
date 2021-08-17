3a1f9cdbbd2b3e92e2c66c1b1217d5981294c570
Khalil Robinson <khalil.robinson@autodesk.om>
2021-08-17 13:39:16 -0400

    still getting setup

---
+++ frontend/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function App(){
+
+    return(
+        <h1>Testing React Code</h1>
+    )
+}
+
+const appDiv = document.getElement("main");
+render (<App />, appDiv);
